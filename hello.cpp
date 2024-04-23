c3c793da6359a2556cab5638609088bff03bac19
886289 <886289@students.wits.ac.za>
2024-04-23 21:53:58 +0200

    Created a new branch which will keep track of a printHello  function

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
-int main() {
+void printHello(string name);
+
+int main(int argc, char* argv[]) {
     cout << "Hello, world!" << endl;
     cout << "Hello, world! Again." << endl;
     cout << "Goodbye world!" << endl;
+    printHello(argv[1]);
     return 0;
+}
+
+void printHello(string name) {
+    cout << "Hello, " << name << "." << endl;
 }
