fe29817ab5c54ee6d4387f7d4fbdd04dfb30907e
886289 <886289@students.wits.ac.za>
2024-04-23 21:26:56 +0200

    Add goodbye from local repo

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
     cout << "Hello, world!" << endl;
     cout << "Hello, world! Again." << endl;
+    cout << "Goodbye from local git repo" << endl;
     return 0;
 }
